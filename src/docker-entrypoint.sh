d6703006923f35f16e6e3e999513a0f20cc4e284
Jackson Veroneze <jackson@jacksonveroneze.com>
2023-03-26 11:04:55 -0300

    feat: ajustes

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-APP=JacksonVeroneze.Ged.Api.dll
+APP=JacksonVeroneze.TemplateWebApi.Api.dll
 OS=$(cat /etc/*-release | egrep "PRETTY_NAME" | cut -d = -f 2 | tr -d '"')
 
 echo "- System Operation: $OS"
-echo "- Run xray-daemon /usr/bin/xray"
-/usr/bin/xray &
-
-echo - Run application $APP
+echo "- Run application: $APP"
+echo "- EnvironmentName: $ASPNETCORE_ENVIRONMENT"
 echo ""
 dotnet $APP
